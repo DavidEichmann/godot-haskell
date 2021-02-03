815b20258569989b4aa4c1f8945028311bd6c4b3
David Eichmann <EichmannD@gmail.com>
2021-02-03 22:13:43 +0000

    Better type errors for (:<)

--- src/Godot/Internal/Dispatch.hs
+++ src/Godot/Internal/Dispatch.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE DataKinds #-}
 module Godot.Internal.Dispatch(HasBaseClass(..),(:<)(..),Signal(..)) where
-import Data.Text (Text)
+import qualified Data.Text
 import GHC.TypeLits as T
 import Godot.Gdnative.Internal.Gdnative hiding (Signal)
 import Language.Haskell.TH
@@ -8,6 +9,7 @@ import Language.Haskell.TH.Datatype
 import Control.Monad
 import Data.List
 import Data.Kind as K
+import Type.Errors
 
 -- | Establishes 'child` as a child of BaseClass child`
 class HasBaseClass child where
@@ -15,14 +17,31 @@ class HasBaseClass child where
   super :: child -> BaseClass child
 
 -- | Subclass relation.
-class parent :< child where
+class parent :< child  where
   upcast :: child -> parent
 
-instance {-# OVERLAPPING #-} a :< a where
-  upcast = id
+instance (CheckInheritance parent child child, parent :<< child) => parent :< child where
+  upcast = upcast'
 
-instance {-# OVERLAPPABLE #-} (HasBaseClass child, parent :< BaseClass child) => parent :< child where
-  upcast = upcast . super
+-- Check inheritance and output a custom type error.
+type family CheckInheritance parent child childOrig :: Constraint where
+  CheckInheritance a a _ = ()
+  CheckInheritance parent child childOrig = IfStuck
+    (BaseClass child)
+    (DelayError (ErrorInheritance parent childOrig))
+    (Pure (CheckInheritance parent (BaseClass child) childOrig))
+type ErrorInheritance parent child =
+  Text "No instance for " :<>: ShowType parent :<>: Text " :< " :<>: ShowType child
 
-newtype Signal a = Signal Text
+-- | Subclass relation without custom type errors.
+class parent :<< child where
+  upcast' :: child -> parent
+
+instance {-# OVERLAPPING #-} a :<< a where
+  upcast' = id
+
+instance  {-# OVERLAPPABLE #-}  (HasBaseClass child, parent :<< BaseClass child) => parent :<< child where
+  upcast' = upcast' . super
+
+newtype Signal a = Signal Data.Text.Text
   deriving (Show, Eq)
